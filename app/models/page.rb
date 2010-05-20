94dc74b387a8820dbc298bd93117f2dfe4f8ba0e
Brian R Zaik <brian.r.zaik@gmail.com>
2010-05-20 15:50:24 -0400

    Built pages/show

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,5 @@
 class Page < ActiveRecord::Base
+  has_many :page_sidebars
   has_many :sidebars, :through => :page_sidebars
   has_many :images, :as => :imageable
   has_many :categories, :through => :category_pages
